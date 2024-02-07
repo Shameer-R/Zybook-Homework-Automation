1567595acb7153db7659f3282413ae834002b0cd
shammy0923 <roseshammy@gmail.com>
2024-02-07 00:03:49 -0500

    Set up 'Get Element' function, start work on login function

--- main.py
+++ main.py
@@ -4,3 +4,23 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
+def getElement(xPath):
+    try:
+        element = WebDriverWait(driver, 10).until(
+            presence_of_element_located(
+                (By.XPATH, xPath))
+        )
+    finally:
+        return element
+def login(driver):
+
+    driver.get("https://www.zybooks.com/")
+
+    loginButton = getElement("/html/body/div[1]/header/div[2]/div/div/nav/div/ul/li[7]/a/span[2]")
+    loginButton.click()
+
+
+with webdriver.Firefox() as driver:
+
+    login(driver)
+
