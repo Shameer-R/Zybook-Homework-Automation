e2af79e649d58f49f56726e5b2003a39cb815813
Shameer-R <roseshammy@gmail.com>
2024-02-07 00:56:06 -0500

    Start course selector function + improve prompts

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
+# Get regular button elements through the xpath
 def getElement(driver, xPath):
     try:
         element = WebDriverWait(driver, 10).until(
@@ -12,6 +13,7 @@ def getElement(driver, xPath):
         )
     finally:
         return element
+
 def login(driver, email, password):
 
     driver.get("https://www.zybooks.com/")
@@ -28,16 +30,38 @@ def login(driver, email, password):
     signInButton = getElement(driver, "/html/body/div[2]/div/div/div[3]/button")
     signInButton.click()
 
+
+
     print("Successfully signed into Zybooks!!!")
+
+def courseSelector(driver):
+
+    try:
+        element = WebDriverWait(driver, 10).until(
+            presence_of_element_located(
+                (By.XPATH, "/html/body/header/div[1]/div/ul/a/li")
+            )
+        )
+    finally:
+
+        instructorSelected = int(input("\nSelect the number corresponding to your instructor: "))
+        index = 0
+        instructorList = driver.find_elements(By.CLASS_NAME, "subheading")
+        for instructor in instructorList:
+            print(instructor.text + " - " + str(index))
+            index += 1
+
+
 
 
 
 def Main():
-    email = input("What's your Zybook email?")
-    password = input("What's your Zybook password?")
+    email = input("Enter your Zybook email: ")
+    password = input("Enter your Zybook password: ")
 
     driver = webdriver.Firefox()
     login(driver, email, password)
+    courseSelector(driver)
 
 
 Main()
