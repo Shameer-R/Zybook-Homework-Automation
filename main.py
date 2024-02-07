57bbc932f94509af5da476f6e870675d9b2703a8
shammy0923 <roseshammy@gmail.com>
2024-02-07 00:15:16 -0500

    Finish login function

--- main.py
+++ main.py
@@ -12,15 +12,31 @@ def getElement(xPath):
         )
     finally:
         return element
-def login(driver):
+def login(driver, email, password):
 
     driver.get("https://www.zybooks.com/")
 
     loginButton = getElement("/html/body/div[1]/header/div[2]/div/div/nav/div/ul/li[7]/a/span[2]")
     loginButton.click()
 
+    emailTextBox = getElement("/html/body/div[2]/div/div/div[1]/div[1]/input")
+    emailTextBox.send_keys(email)
+
+    passwordTextBox = getElement("/html/body/div[2]/div/div/div[1]/div[2]/input")
+    passwordTextBox.send_keys(password)
+
+    signInButton = getElement("/html/body/div[2]/div/div/div[3]/button")
+    signInButton.click()
+
+    print("Successfully signed into Zybooks!")
+
+
+
+email = input("What's your Zybook email?")
+password = input("What's your Zybook password?")
+
 
 with webdriver.Firefox() as driver:
 
-    login(driver)
+    login(driver, email, password)
 
