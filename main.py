fb1269119d01f0578d3a0bfadf2dc8312e2fe57c
shammy0923 <roseshammy@gmail.com>
2024-02-07 00:18:23 -0500

    Make 'Main' function + Make a 'driver' parameter for 'Get Element' function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
-def getElement(xPath):
+def getElement(driver, xPath):
     try:
         element = WebDriverWait(driver, 10).until(
             presence_of_element_located(
@@ -16,27 +16,28 @@ def login(driver, email, password):
 
     driver.get("https://www.zybooks.com/")
 
-    loginButton = getElement("/html/body/div[1]/header/div[2]/div/div/nav/div/ul/li[7]/a/span[2]")
+    loginButton = getElement(driver, "/html/body/div[1]/header/div[2]/div/div/nav/div/ul/li[7]/a/span[2]")
     loginButton.click()
 
-    emailTextBox = getElement("/html/body/div[2]/div/div/div[1]/div[1]/input")
+    emailTextBox = getElement(driver, "/html/body/div[2]/div/div/div[1]/div[1]/input")
     emailTextBox.send_keys(email)
 
-    passwordTextBox = getElement("/html/body/div[2]/div/div/div[1]/div[2]/input")
+    passwordTextBox = getElement(driver, "/html/body/div[2]/div/div/div[1]/div[2]/input")
     passwordTextBox.send_keys(password)
 
-    signInButton = getElement("/html/body/div[2]/div/div/div[3]/button")
+    signInButton = getElement(driver, "/html/body/div[2]/div/div/div[3]/button")
     signInButton.click()
 
     print("Successfully signed into Zybooks!")
 
 
 
-email = input("What's your Zybook email?")
-password = input("What's your Zybook password?")
+def Main():
+    email = input("What's your Zybook email?")
+    password = input("What's your Zybook password?")
 
-
-with webdriver.Firefox() as driver:
-
+    driver = webdriver.Firefox()
     login(driver, email, password)
 
+
+Main()
